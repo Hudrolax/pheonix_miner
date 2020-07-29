726848c4d9dd8b0cd8901f44461ca21d3e4612e1
hudro <hudro795@gmail.com>
2020-07-29 18:36:25 +0300

    init

--- start_miner.sh
+++ start_miner.sh
@@ -3,4 +3,7 @@
 #
 
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+pause=5
+echo "sleep $pause sec. before start miner"
+sleep $pause
 /root/pheonix/PhoenixMiner -pool coinotron.com:3344 -wal Hudro.zeon
