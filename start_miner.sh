6f00aa8e168a4529a5f617faf2f2c863de0d8dce
hudro <hudro795@gmail.com>
2020-07-29 18:27:36 +0300

    init

---
+++ start_miner.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETH
+#
+
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+/root/pheonix/PhoenixMiner -pool coinotron.com:3344 -wal Hudro.zeon
